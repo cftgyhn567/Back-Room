69fea988d2218c8617edbef26ea87fe07e6e8cb2
cftgyhn567 <jesus978645312john@gmail.com>
2021-03-10 15:03:53 +0800

    v0

--- app/src/main/java/com/example/backroom/MainActivity.java
+++ app/src/main/java/com/example/backroom/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.backroom;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.v("brad", "onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.v("brad", "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.v("brad", "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.v("brad", "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.v("brad", "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.v("brad", "onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.v("brad", "onRestart");
+    }
+
+    @Override
+    public void finish() {
+        //super.finish();
+        Toast.makeText(this, "再次點擊後退出", Toast.LENGTH_SHORT).show();
     }
 }
